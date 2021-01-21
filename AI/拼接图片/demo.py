7aab775f665615b14a780c85111e6d9a72fa4f78
Nooob-dy <goodboy-dy@outlook.com>
2021-01-21 21:52:56 +0800

    2021年1月21日21:52:52

--- AI/拼接图片/demo.py
+++ AI/拼接图片/demo.py
@@ -3,7 +3,7 @@ import argparse
 from itertools import product
 
 import cv2
-import numpy as py
+import numpy as np # 数组运算
 from tqdm import tqdm
 
 '''
@@ -32,18 +32,48 @@ def parsArgs():
 def readSourceImage(sourcepath,blocksize):
     print('开始读取图像')
     # 符合颜色要求图像
-    sourceimage = []
+    sourceimages = []
     # 平均颜色列表
     avgcolors = []
 
-    for path in tqdm(range(glob.glob("{}/*jpg".format(sourcepath)))): # 读取文件路径 tqdm为进度条
+    for path in tqdm(glob.glob("{}/*.jpg".format(sourcepath))): # 读取文件路径 tqdm为进度条
         image = cv2.imread(path, cv2.IMREAD_COLOR)
         if image.shape[-1] != 3: # RGB三原色为三, 不为三则不要
             continue
 
         image = cv2.resize(image, (blocksize, blocksize)) # 缩放尺寸大小
+        avgcolor = np.sum(np.sum(image, axis=0), axis=0)/(blocksize, blocksize)
+
+        sourceimages.append(image)
+        avgcolors.append(avgcolor)
+    print('读取结束')
+    return sourceimages, np.array(avgcolors)
+
+
+def main(args):
+    targetimage = cv2.imread(args.targetpath)
+    outputimage = np.zeros(targetimage.shape, np.uint8)
+
+    sourceimages, avgcolors = readSourceImage(args.sourcepath, args.blocksize)
+
+    print('开始制作')
+
+    for i, j in tqdm(product(range(int(targetimage.shape[1]/args.blocksize)),
+                             range(int(targetimage.shape[0]/args.blocksize)))):
+        block = targetimage[j * args.blocksizeL(j + 1) * args.blocksize, i * args.blocksize:(i + 1) * args.blocksize, :]
+        avgcolor = np.sum(np.sum(block, axis=0), axis=0) / (args.blocksize * args.blocksize)
+        distances = np.linalg.norm(avgcolor - avgcolors, axis=1)
+
+        idx = np.argmin(distances)
+        outputimage[j * args.blocksizeL(j + 1) * args.blocksize, i * args.blocksize:(i + 1) * args.blocksize, :] = sourceimages[idx]
+
+    cv2.imwrite(args.outputpath, outputimage)
+    cv2.imshow('result', outputimage)
+    print('Done')
 
 
 
-image = cv2.imread('sourceimages/desktop1.jpg')
-print(image)
+
+if __name__ == '__main__':
+
+    main(parsArgs())
